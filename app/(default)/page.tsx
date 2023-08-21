28e94affc15b16c77dee6604c0c6c64c7d1423da
Rohan Nagabhirava <rohanbn2@illinois.edu>
2023-08-20 19:16:14 -0500

    adding node.js to folder

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -13,10 +13,12 @@ export default function Home() {
   return (
     <>
       <Hero />
+      {/*
       <Features />
       <Zigzag />
       <Testimonials />
       <Newsletter />
+  */}
     </>
   )
 }
